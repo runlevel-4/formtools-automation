cc103be9cf9fbb264d2c87c56b9cf315a8f2a925
runlevel4 <90661907+runlevel-4@users.noreply.github.com>
2021-11-06 10:38:25 -0400

    Update mysql_setup.sql

--- mysql_setup.sql
+++ mysql_setup.sql
@@ -1,4 +1,3 @@
 CREATE DATABASE formtools;
 CREATE USER 'formtools'@'localhost' IDENTIFIED BY 'formtools';
 GRANT ALL PRIVILEGES ON formtools.* TO 'formtools'@'localhost';
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'toor';
